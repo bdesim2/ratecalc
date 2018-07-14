9130a70c97b10cd00ea3ecf37d46f60121b3726d
Brian DeSimone <bdesim2@gmail.com>
2018-07-14 00:51:32 -0500

    making sure we have proper resource package names in the configs

--- src/main/java/com/ratecalc/RateCalcApplication.java
+++ src/main/java/com/ratecalc/RateCalcApplication.java
@@ -24,9 +24,9 @@ public class RateCalcApplication extends Application {
         BeanConfig beanConfig = new BeanConfig();
         beanConfig.setVersion("1.0.2");
         beanConfig.setSchemes(new String[]{"http"});
-        beanConfig.setHost("localhost:80");
+        beanConfig.setHost("localhost:8080");
         beanConfig.setBasePath("/api");
-        beanConfig.setResourcePackage("com.ratecalculator");
+        beanConfig.setResourcePackage("com.ratecalc");
         beanConfig.setScan(true);
     }
 
