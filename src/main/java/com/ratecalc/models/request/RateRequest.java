aa399ca89d326cf3d4bcc91cbe7b750963de28d9
Brian DeSimone <bdesim2@gmail.com>
2018-07-14 16:56:03 -0500

    Adding a raterequest pojo that supports xml and json

--- src/main/java/com/ratecalc/models/request/RateRequest.java
+++ src/main/java/com/ratecalc/models/request/RateRequest.java
@@ -1,7 +1,51 @@
 package com.ratecalc.models.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
- * Created by bdesi on 7/14/2018.
+ * This class will represent the pojo for rate requests
+ * Two parameters will be used here for JSON and XML requests
+ * startRate and endRate
+ *
+ * @Author Brian DeSimone
+ * @Date 07/14/2018
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
+@XmlRootElement(name = "rate")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class RateRequest {
+
+    @XmlElement(name = "start")
+    @JsonProperty(value = "start")
+    private String startRate;
+
+    @XmlElement(name = "end")
+    @JsonProperty(value = "end")
+    private String endRate;
+
+    public RateRequest(){
+
+    }
+
+    public String getStartRate() {
+        return startRate;
+    }
+
+    public void setStartRate(String startRate) {
+        this.startRate = startRate;
+    }
+
+    public String getEndRate() {
+        return endRate;
+    }
+
+    public void setEndRate(String endRate) {
+        this.endRate = endRate;
+    }
 }
