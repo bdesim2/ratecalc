015fffde6d38a581d5e36b8cbdc9df20bb1a2cc8
Brian DeSimone <bdesim2@gmail.com>
2018-07-14 00:51:44 -0500

    adding an isAlive controller so we can have a healthcheck api

---
+++ src/main/java/com/ratecalc/controllers/isAliveController.java
@@ -0,0 +1,10 @@
+package com.ratecalc.controllers;
+
+/**
+ * This class provides a simple health check API that will verify the server is functioning
+ *
+ * @Author Brian DeSimone
+ * @Date 07/13/2018
+ */
+public class isAliveController {
+}
