39c4b10fcf8926076d6a3480ebdf3b4e97bd4620
Brian DeSimone <bdesim2@gmail.com>
2018-07-15 11:18:20 -0500

    adding in the rate and rates pojos for reading in the json config

--- src/main/java/com/ratecalc/models/rate/Rate.java
+++ src/main/java/com/ratecalc/models/rate/Rate.java
@@ -1,7 +1,47 @@
 package com.ratecalc.models.rate;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
- * Created by bdesi on 7/14/2018.
+ * Class to create the pojo from our parking_rates.json
+ *
+ * @Author Brian DeSimone
+ * @Date 07/14/2018
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Rate {
+
+    @JsonProperty
+    private String days;
+
+    @JsonProperty
+    private String times;
+
+    @JsonProperty
+    private int price;
+
+    public String getDays() {
+        return days;
+    }
+
+    public void setDays(String days) {
+        this.days = days;
+    }
+
+    public String getTimes() {
+        return times;
+    }
+
+    public void setTimes(String times) {
+        this.times = times;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
